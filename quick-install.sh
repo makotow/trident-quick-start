19fc223cbd7aaff81743340e37a4d8bedfc196ae
Makoto Watanabe <makoto.wata@gmail.com>
2019-08-19 11:46:44 +0900

    Logic enahncement.

--- quick-install.sh
+++ quick-install.sh
@@ -3,14 +3,14 @@
 VERSION=19.07.0
 TEMPLATE_FILE=backend-ontap-nas.json
 
-wget https://github.com/NetApp/trident/releases/download/v${VERSION}/trident-installer-${VERSION}.tar.gz
-tar -xf trident-installer-${VERSION}.tar.gz
+if [ ! -f trident-installer-${VERSION}.tar.gz ]; then
+    wget https://github.com/NetApp/trident/releases/download/v${VERSION}/trident-installer-${VERSION}.tar.gz
+    tar -xf trident-installer-${VERSION}.tar.gz
+fi
 
 cd trident-installer || exit
 kubectl create ns trident
 ./tridentctl install -n trident
-
-
 cp sample-input/${TEMPLATE_FILE} backend.json
 vi backend.json
 
